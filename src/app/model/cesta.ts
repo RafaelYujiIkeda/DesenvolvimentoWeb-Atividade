a57656e17c3ee730a21b344ff79d91101cccffd3
Rafaelyujiikeda <rafaelikeda1620@gmail.com>
2026-09-16 13:37:02 -0300

    quarto commit

--- src/app/model/cesta.ts
+++ src/app/model/cesta.ts
@@ -1,1 +1,24 @@
-export class Cesta {}
+import { ItemCesta } from './item-cesta';
+
+export class Cesta {
+
+  itens: ItemCesta[] = [];
+
+  adicionar(item: ItemCesta) {
+    this.itens.push(item);
+  }
+
+  remover(codigo: number) {
+    this.itens = this.itens.filter(
+      item => item.codigo !== codigo
+    );
+  }
+
+  calcularTotal(): number {
+    return this.itens.reduce(
+      (total, item) => total + (item.valor * item.quantidade),
+      0
+    );
+  }
+
+}
--- src/app/model/item-cesta.ts
+++ src/app/model/item-cesta.ts
@@ -1,1 +1,20 @@
-export class ItemCesta {}
+export class ItemCesta {
+
+  codigo: number;
+  nome: string;
+  valor: number;
+  quantidade: number;
+
+  constructor(
+    codigo: number,
+    nome: string,
+    valor: number,
+    quantidade: number
+  ) {
+    this.codigo = codigo;
+    this.nome = nome;
+    this.valor = valor;
+    this.quantidade = quantidade;
+  }
+
+}
